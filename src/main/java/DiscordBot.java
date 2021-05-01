5a4f74433e24953a5235c82d80f8e407d76a9430
emath01 <emath01@students.cs.ubc.ca>
2021-04-30 22:52:06 -0700

    DiscordBot branch.

---
+++ src/main/java/DiscordBot.java
@@ -0,0 +1,13 @@
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class DiscordBot extends ListenerAdapter {
+    private final String token = "TOKEN"; //replace with TOKEN
+    private JDABuilder builder;
+
+    public DiscordBot() {
+
+    }
+}
