63ab5643a40f8e4a8a6bac64d41787c33472f607
yeojunh <yeojunhann@gmail.com>
2021-05-01 10:39:24 -0700

    Removed extra library lines

--- src/main/java/about.java
+++ src/main/java/about.java
@@ -1,12 +1,6 @@
 import events.aboutevent;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class about {
 
