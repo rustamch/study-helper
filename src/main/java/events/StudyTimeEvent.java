5593282353f4b265b11771c7305960499d44cac5
yeojunh <yeojunhann@gmail.com>
2021-05-01 16:01:12 -0700

    Comment out main methods temporarily

--- src/main/java/events/StudyTimeEvent.java
+++ src/main/java/events/StudyTimeEvent.java
@@ -3,6 +3,7 @@ package events;
 import java.time.Instant;
 import java.time.Duration;
 
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
@@ -15,16 +16,18 @@ public class StudyTimeEvent {
     Instant start;
     Instant finish;
 
+    //inVoiceChannel();
+    /*
     public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
-        start = Instant.now();
+        //start = Instant.now();
         textChannel = event.getGuild().getTextChannelsByName("general", true).get(0);
         textChannel.sendMessage("User has joined vc!").queue();
     }
 
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
-        finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
+        //finish = Instant.now();
+        //long timeElapsed = Duration.between(start, finish).toMillis();
         textChannel.sendMessage("User has left vc after " + timeElapsed*1000 + " seconds!");
         textChannel.sendMessage("The methods are running, but the time counter is not.");
-    }
+    }*/
 }
