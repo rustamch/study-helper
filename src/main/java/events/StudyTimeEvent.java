740ff5f53c135d2aec91880417d983ae9f390ec7
DoraQwQ <dora.qy86@gmail.com>
2021-05-02 13:29:00 -0700

    stored member and start times in HashMap

--- src/main/java/events/StudyTimeEvent.java
+++ src/main/java/events/StudyTimeEvent.java
@@ -2,6 +2,8 @@ package events;
 
 import java.time.Instant;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
@@ -11,34 +13,50 @@ import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import persistence.JSONReader;
+import persistence.JSONWriter;
 
 public class StudyTimeEvent extends ListenerAdapter {
+    private Map<String, Instant> membersInVC = new HashMap<>();
 
     TextChannel textChannel;
-    Instant start;
     Instant finish;
     String memberID;
-    String nickname;
     long timeStudied;
 
+
     public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
-        start = Instant.now();
+        Member m = event.getMember();
+        Instant start = Instant.now();
         textChannel = event.getGuild().getTextChannelsByName("general", true).get(0);
-        nickname = event.getMember().getEffectiveName();
-        textChannel.sendMessage(nickname + " has started studying!").queue();
+        membersInVC.put(m.getId(), start);
+        textChannel.sendMessage(m.getEffectiveName() + " has started studying!").queue();
     }
 
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
         finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
         memberID = event.getMember().getId();
-        if(timeElapsed/1000 > 3600)
-            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed/1000/60/60 + "** hours" +
-                    "and " + timeElapsed/1000/60 % 3600 + "minutes!").queue();
-        else if(timeElapsed/1000 > 60)
-            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed/1000/60 + "** minutes!").queue();
+        Instant start = membersInVC.get(memberID);
+        long timeElapsed = Duration.between(start, finish).toMillis();
+
+        sendTimeElapsedMessage(timeElapsed);
+        storeElapsedTime(memberID, timeElapsed);
+    }
+
+    private void storeElapsedTime(String memberID, long timeElapsed) {
+        JSONReader reader = new JSONReader("./times.json");
+        JSONWriter writer = new JSONWriter("./times.json");
+
+
+    }
+
+    private void sendTimeElapsedMessage(long timeElapsed) {
+        if(timeElapsed /1000 > 3600)
+            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed /1000/60/60 + "** hours" +
+                    "and " + timeElapsed /1000/60 % 3600 + "minutes!").queue();
+        else if(timeElapsed /1000 > 60)
+            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed /1000/60 + "** minutes!").queue();
         else
-            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed/1000 + "** seconds!").queue();
-        timeStudied = timeElapsed;
+            textChannel.sendMessage("<@" + memberID + ">" +" has studied for **" + timeElapsed /1000 + "** seconds!").queue();
     }
 }
