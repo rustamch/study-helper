e7f1fc95971064db2a31be7a77440d33486115c0
yeojunh <yeojunhann@gmail.com>
2021-05-01 18:50:46 -0700

    Change colour, list alternate ymd style

--- src/main/java/events/AboutEvent.java
+++ src/main/java/events/AboutEvent.java
@@ -2,19 +2,20 @@ package events;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class AboutEvent extends ListenerAdapter {
-
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+    public void onMessageReceived(MessageReceivedEvent event) {
         String messageSent = event.getMessage().getContentRaw();
-        if(messageSent.equalsIgnoreCase("!about")){
+        if(messageSent.equalsIgnoreCase("!about")) {
             EmbedBuilder about = new EmbedBuilder();
-            about.setTitle("Study Hall Bot Information");
-            about.setDescription("A bot to help students focus!");
-            about.addField("Creators", "A couple UBC 1st year students!", false);
-            about.setColor(0xA9927D);
+            about.setTitle("🌿 Study Hall Bot Information");
+            about.setDescription("A bot to help YOU study better :D");
+            about.addField("Creators", "A couple UBC 1st year students in *RUHacks 2021*!", false);
+            about.addField("Birthday Commands", "!bday setbday year/month/date or year.month.date\n!bday lookup @<user>", false);
+            about.setColor(0x9CD08F);
 
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage(about.build()).queue();
