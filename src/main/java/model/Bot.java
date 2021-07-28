64151947983ec116aea1a18c35d47d18e1f293c1
Rustam <rustamnassyrov02@gmail.com>
2021-07-27 23:58:47 -0700

    Forgot to stage the file.

--- src/main/java/model/Bot.java
+++ src/main/java/model/Bot.java
@@ -4,6 +4,7 @@ import javax.security.auth.login.LoginException;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
 
 
 public class Bot {
@@ -12,6 +13,7 @@ public class Bot {
     public static void main(String[] args) throws LoginException {
         API = new DiscordApiBuilder()
         .setToken(System.getenv("discord_token"))
+        .setAllIntentsExcept(Intent.GUILD_PRESENCES, Intent.GUILD_WEBHOOKS)
         .login().join();
         API.addMessageCreateListener(new MessageListener());
         API.addListener(new StudyTimeEvent());
