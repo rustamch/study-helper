73d91d61d8e99e17b04e94032e9b6e74e7abcd2e
Rustam <rustamnassyrov02@gmail.com>
2021-08-25 19:03:24 -0700

    Added study role auto-configuration.

--- src/main/java/events/ConfigCommand.java
+++ src/main/java/events/ConfigCommand.java
@@ -1,34 +1,75 @@
 package events;
 
+import org.javacord.api.entity.permission.Permissions;
+import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class ConfigCommand implements BotMessageEvent {
 
     @Override
     public void invoke(MessageCreateEvent event, String[] content) {
+        if (event.getMessageAuthor().isServerAdmin()) {
+            if (content[0] == null) {
+                handleSubCommand(event, content);
+            } else {
+                event.getChannel().sendMessage("Please enter a valid subcommand!" +
+                        "Please use !config help to learn more.");
+            }
+        } else {
+            event.getChannel().sendMessage("Only server admins can use config commands!");
+        }
+    }
+
+    private void handleSubCommand(MessageCreateEvent event, String[] content) {
         String subCommand = content[0];
         switch (subCommand) {
-            case ("set"):
-                if (event.getMessageAuthor().isServerAdmin()) {
-                    event.getServer().ifPresent(server -> {
-                        try {
-                            long roleId = Long.parseLong(content[1]);
-                            server.getRoleById(roleId).ifPresentOrElse(role -> {
-                                long serverId = server.getId();
-                                ServerConfig.setStudyRoleForServer(serverId, roleId);
-                                event.getChannel().sendMessage("Successfully set the study role to be " +
-                                        role.getName() + " role !");
-                            }, () -> event.getChannel()
-                                    .sendMessage("It looks like id you provided isn't associated" +
-                                            " with any role on this server!"));
-                        } catch (NumberFormatException e) {
-                            event.getChannel()
-                                    .sendMessage("It looks like you have put a word instead of role id!" +
-                                            "Try sending !config set roleId");
-                        }
-                    });
-                }
+            case ("studyrole"):
+                handleStudyRoleCommands(event, content);
                 break;
         }
     }
+
+    private void handleStudyRoleCommands(MessageCreateEvent event, String[] content) {
+        String studySubCommand = content[1];
+            event.getServer().ifPresent(server -> {
+                switch (studySubCommand) {
+                    case ("set"):
+                        setStudyRole(event,content,server);
+                        break;
+                    case ("auto-config"):
+                        createStudyRole(event,content,server);
+                       break;
+                }
+            });
+    }
+
+    private void createStudyRole(MessageCreateEvent event, String[] content, Server server) {
+        server.createRoleBuilder().setName("studying").create().thenAccept(role -> {
+            ServerConfig.setStudyRoleForServer(server.getId(), role.getId());
+            server.getChannelCategories().forEach(channelCategory -> {
+                channelCategory.createUpdater().addPermissionOverwrite(role,
+                        Permissions.fromBitmask(0x0, 0x0000010400));
+            });
+        });
+    }
+
+    private void setStudyRole(MessageCreateEvent event, String[] content, Server server) {
+        try {
+            long roleId = Long.parseLong(content[1]);
+            server.getRoleById(roleId).ifPresentOrElse(role -> {
+                long serverId = server.getId();
+                ServerConfig.setStudyRoleForServer(serverId, roleId);
+                event.getChannel().sendMessage("Successfully set the study role to be " +
+                        role.getName() + " role !");
+            }, () -> event.getChannel()
+                    .sendMessage("It looks like id you provided isn't associated" +
+                            " with any role on this server!"));
+        } catch (NumberFormatException e) {
+            event.getChannel()
+                    .sendMessage("It looks like you have put a word instead of role id!" +
+                            "Try sending !config set roleId");
+        }
+    }
+
 }
+
