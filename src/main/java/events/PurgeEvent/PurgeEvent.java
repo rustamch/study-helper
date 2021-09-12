3f496112ad41298a20f01a0460e5a96a542903ed
Rustam <rustamnassyrov02@gmail.com>
2021-09-11 19:37:16 -0700

    Fixed the delete command permissions.

--- src/main/java/events/PurgeEvent/PurgeEvent.java
+++ src/main/java/events/PurgeEvent/PurgeEvent.java
@@ -9,10 +9,14 @@ public class PurgeEvent implements BotMessageEvent {
 
     @Override
     public void invoke(MessageCreateEvent event, String[] content) {
-        if (content.length == 0) {
-            return;
+        if (event.getMessageAuthor().isServerAdmin()) {
+            if (content.length == 0) {
+                event.getChannel().sendMessage("Please specify number of messages you want to delete!");
+            } else {
+                handleSubCommand(event, content);
+            }
         } else {
-            handleSubCommand(event, content);
+            event.getChannel().sendMessage("You need to be admin to use this command!");
         }
     }
 
@@ -21,14 +25,10 @@ public class PurgeEvent implements BotMessageEvent {
         TextChannel channel = event.getChannel();
         if (subCommand.equalsIgnoreCase("all"))  {
             channel.sendMessage("You have asked to delete all the messages!");
-            channel.getMessagesWhile(message -> {
-                return true;
-            }).thenAccept(messages -> channel.deleteMessages(messages));
+            channel.getMessagesWhile(message -> true).thenAccept(channel::deleteMessages);
         } else if (subCommand.matches("(\\d+)")) {
             int num = Integer.parseInt(subCommand);
-            channel.getMessages(num + 2).thenAccept(msgs -> {
-                channel.deleteMessages(msgs);
-            });
+            channel.getMessages(num + 2).thenAccept(channel::deleteMessages);
         }
     }
 }
