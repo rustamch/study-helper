a92aaa9c9733c49e8a17894d98b5a88fcac8f388
Rustam <rustamnassyrov02@gmail.com>
2021-09-24 12:51:51 -0700

    Fix the studyTimeRecord table.

--- src/main/java/model/Bot.java
+++ src/main/java/model/Bot.java
@@ -4,16 +4,31 @@ import events.MessageDeleteListener;
 import events.ReactionEvent.MessageReactionListener;
 import events.StudyTimeEvent.StudyTimeLogger;
 
+import events.StudyTimeEvent.StudyTimeRecord;
+import org.bson.Document;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
 import org.javacord.api.util.logging.FallbackLoggerConfiguration;
+import persistence.DBReader;
+import persistence.DBWriter;
+
+import java.util.function.Consumer;
 
 
 public class Bot {
     public static DiscordApi API;
 
     public static void main(String[] args) {
+        String COLLECTION_NAME = "study_times";
+        DBReader reader = new DBReader(COLLECTION_NAME);
+        DBWriter writer = new DBWriter(COLLECTION_NAME);
+        reader.loadAllDocuments().forEach((Consumer<? super Document>) document -> {
+            document.put(StudyTimeRecord.GLOBAL_STUDY_TIME_KEY,document.getLong("study_time"));
+            document.put(StudyTimeRecord.WEEKLY_STUDY_TIME_KEY,0);
+            document.remove("study_time");
+            writer.saveDocument(document);
+        });
         FallbackLoggerConfiguration.setDebug(true);
         FallbackLoggerConfiguration.setTrace(true);
         API = new DiscordApiBuilder()
